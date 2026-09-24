071106f862b41e1119bc217f5506dad35d1cad9e
Daouda ZONGO <daoudaz6666@gmail.com>
2026-09-24 11:29:19 +0000

    Ajout module configuration CRM Box Africa

---
+++ box_africa_crm_setup/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Box Africa - Configuration CRM',
+    'version': '1.0',
+    'depends': ['crm', 'sale', 'account'],
+    'data': [
+        'data/res_users_data.xml',
+        'data/crm_team_data.xml',
+        'data/crm_stage_data.xml',
+        'data/product_data.xml',
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
